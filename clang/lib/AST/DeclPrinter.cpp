3fca64641d79fe51aaea1fca83bc9743dede9945
William S. Moses <gh@wsmoses.com>
2020-04-06 12:40:49 -0400

    Handle decl printing if noerror exists

--- clang/lib/AST/DeclPrinter.cpp
+++ clang/lib/AST/DeclPrinter.cpp
@@ -686,7 +686,7 @@ void DeclPrinter::VisitFunctionDecl(FunctionDecl *D) {
       FT = dyn_cast<FunctionProtoType>(AFT);
 
     Proto += "(";
-    
+
     if (Policy.nameMangler) {
         if (auto M = dyn_cast<CXXMethodDecl>(D)) {
             if (M->isInstance()) {
@@ -758,7 +758,7 @@ void DeclPrinter::VisitFunctionDecl(FunctionDecl *D) {
           Proto += FT->getExceptionType(I).getAsString(SubPolicy);
         }
       Proto += ")";
-    } else if (FT && isNoexceptExceptionSpec(FT->getExceptionSpecType())) {
+    } else if (!Policy.handleSubType && FT && isNoexceptExceptionSpec(FT->getExceptionSpecType())) {
       Proto += " noexcept";
       if (isComputedNoexcept(FT->getExceptionSpecType())) {
         Proto += "(";
